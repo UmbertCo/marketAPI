50955d9ef513edff24d7fc12555b9d35ea6de548
umbertco <humbertohcota@gmail.com>
2024-05-21 09:58:21 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from 'cors'; // Importa 'cors' como un módulo ES
 import postRoutes from "./routes/post.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:8100', // Origen permitido
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    credentials: true // Si necesitas enviar cookies
+}));
 
 app.use(express.json());
 app.use(postRoutes);
 app.use(userRoutes);
 app.use(authRoutes);
+
 export default app;
